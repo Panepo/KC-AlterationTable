fa805fa12e7589edc13322ae1a1286dab2b42a7a
Panepo <whisper.ko@gmail.com>
2016-02-04 15:29:58 +0800

    [modify function]
    AltList: remove not used calculations

--- js/components/AltList.react.js
+++ js/components/AltList.react.js
@@ -29,39 +29,37 @@ var AltList = React.createClass({
 		var CheckString = "";
 		var KeyString = "";
 
-		for ( var i = 0; i < this.props.list.length; i++) {
-			TableData[i] = [];
-			for ( var j = 0; j < this.props.list[i].length; j++) {
-				for ( var k = 0; k < Constants.checkboxlist.length; k++ ) {
-					if ( this.props.list[i][j][0] == Constants.checkboxlist[k]) {
-						ImgString = "./img/sit" + (k+1) + ".png";
-					}
+		TableData[this.props.day] = [];
+		for ( var j = 0; j < this.props.list[this.props.day].length; j++) {
+			for ( var k = 0; k < Constants.checkboxlist.length; k++ ) {
+				if ( this.props.list[this.props.day][j][0] == Constants.checkboxlist[k]) {
+					ImgString = "./img/sit" + (k+1) + ".png";
 				}
-				
-				for (var k = 0; k < Constants.AAlist.length; k++ ) {
-					if ( this.props.list[i][j][1] == Constants.AAlist[k]) {
-						ImgString = "./img/sit0.png";
-					}
-				}
-				
-				CheckString = this.props.tableId + " row" + i.toString() + j.toString();
-				KeyString = this.props.tableId + i.toString() + j.toString();
-				
-				TableCont = (
-					<tr key={KeyString} id={KeyString} >
-					<td className="th0">
-						<label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect mdl-data-table__select" htmlFor={CheckString}>
-							<input type="checkbox" id={CheckString} className="mdl-checkbox__input" onChange={this.handleChange}/>
-						</label>
-					</td>
-					<td className="th1"><img src={ImgString} height="30" width="30"/></td>
-					<td id={KeyString} className="th2">{this.props.list[i][j][0]}</td>
-					<td id={KeyString} className="th3">{this.props.list[i][j][1]}</td>
-					<td id={KeyString} className="th4 mdl-data-table__cell--non-numeric">{this.props.list[i][j][2]}</td>
-					</tr>
-				);
-				TableData[i].push(TableCont);
 			}
+			
+			for (var k = 0; k < Constants.AAlist.length; k++ ) {
+				if ( this.props.list[this.props.day][j][1] == Constants.AAlist[k]) {
+					ImgString = "./img/sit0.png";
+				}
+			}
+			
+			CheckString = this.props.tableId + " row" + this.props.day.toString() + j.toString();
+			KeyString = this.props.tableId + this.props.day.toString() + j.toString();
+			
+			TableCont = (
+				<tr key={KeyString} id={KeyString} >
+				<td className="th0">
+					<label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect mdl-data-table__select" htmlFor={CheckString}>
+						<input type="checkbox" id={CheckString} className="mdl-checkbox__input" onChange={this.handleChange}/>
+					</label>
+				</td>
+				<td className="th1"><img src={ImgString} height="30" width="30"/></td>
+				<td id={KeyString} className="th2">{this.props.list[this.props.day][j][0]}</td>
+				<td id={KeyString} className="th3">{this.props.list[this.props.day][j][1]}</td>
+				<td id={KeyString} className="th4 mdl-data-table__cell--non-numeric">{this.props.list[this.props.day][j][2]}</td>
+				</tr>
+			);
+			TableData[this.props.day].push(TableCont);
 		}
 		
 		if ( this.props.display === 0 || this.props.list[this.props.day].length === 0 ) {
