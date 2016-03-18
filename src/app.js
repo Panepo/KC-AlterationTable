6359b3398c57716a6f129926f1bc74dad83a58c2
Panepo <whisper.ko@gmail.com>
2016-03-18 17:24:15 +0800

    [remove old files]
    remove old files

--- src/app.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import KCFactoryApp from "./components/KCFactoryApp.react";
-
-ReactDOM.render(
-  <KCFactoryApp />,
-  document.getElementById("KCFactoryApp")
-);
